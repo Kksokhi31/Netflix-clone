d1475044742ef17cbd5fa45fbfed0dd07d0ded51
Kamal Kaur <kamalkaur@Kamals-MacBook-Pro.local>
2024-03-03 22:21:38 +0530

    NextAuth, Prisma and Mongo Setup, establishing connections with DB. Logging in and register

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,17 +1,50 @@
 import Input from "@/components/input";
 import { useCallback, useState } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
+import { signIn } from "next-auth/react";
 
 const Auth = () => {
-const [email, setEmail] = useState('');
-const [name, setName] = useState('');
-const [password, setPassword] = useState('');
+    const router = useRouter();
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [variant, setVariant] = useState('login');
 
-const [variant, setVariant] = useState('login');
-const toggleVariant = useCallback(() => {
-    setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login') 
-}, [])
+    const toggleVariant = useCallback(() => {
+        setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
+    }, []);
 
 
+    const login = useCallback(async () => {
+        try {
+            await signIn('credentials', {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/'
+            });
+            router.push('/');
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }, [email,password, router])
+    
+    const register = useCallback(async () => {
+        try {
+            await axios.post('/api/register', {
+                email,
+                name,
+                password
+            });
+            login();
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }, [email, name, password, login])
+    
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -26,29 +59,29 @@ const toggleVariant = useCallback(() => {
                         <div className="flex flex-col gap-4">
                             {variant === 'register' && (
                                 <Input
-                                label="Username"
-                                onChange={(ev: any) => setName(ev.target.value)}
-                                id="name"
-                                type="name"
-                                value={name}
+                                    label="Username"
+                                    onChange={(ev: any) => setName(ev.target.value)}
+                                    id="name"
+                                    type="name"
+                                    value={name}
                                 />
                             )}
                             <Input
-                            label="Email"
-                            onChange={(ev: any) => setEmail(ev.target.value)}
-                            id="email"
-                            type="email"
-                            value={email}
+                                label="Email"
+                                onChange={(ev: any) => setEmail(ev.target.value)}
+                                id="email"
+                                type="email"
+                                value={email}
                             />
                             <Input
-                            label="Password"
-                            onChange={(ev: any) => setPassword(ev.target.value)}
-                            id="password"
-                            type="password"
-                            value={password}
+                                label="Password"
+                                onChange={(ev: any) => setPassword(ev.target.value)}
+                                id="password"
+                                type="password"
+                                value={password}
                             />
                         </div>
-                        <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                        <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                             {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
                         <p className="text-neutral-500 mt-12">
